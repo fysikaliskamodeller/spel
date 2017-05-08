51fe5b981c82049be33453162cd7048ae395ec44
Axel Enström <axeens@gmail.com>
2017-05-08 21:44:42 +0200

    Add files via upload
    Klassen för att göra en popup

---
+++ PopUp.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Axel Enström on 2017-05-08.
+ */
+import javax.swing.JOptionPane;
+import java.awt.*;
+
+public class PopUp {
+    public static void main(String args[]) {
+
+        JOptionPane pop = new JOptionPane();
+        pop.showMessageDialog(null, "Grattis du klarade spelet!");
+
+    }
+
+
+}
