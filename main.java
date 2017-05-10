3751fa5b28b2c38ab9feded37c75ccf8e1326376
Axel Enström <axeens@gmail.com>
2017-05-10 09:06:47 +0200

    Add files via upload

--- main.java
+++ main.java
@@ -1,25 +1,35 @@
-package Game_1;
+
 
 import java.awt.Color;
 
 import javax.swing.JFrame;
 
 public class main {
-	
-	
-	public static void main(String [] args)
-	{
-		JFrame f = new JFrame();
-		TestJPanel t = new TestJPanel();
+
+	static JFrame f = new JFrame();
+	static TestJPanel t = new TestJPanel();
+
+
+	public static void setup(){
+
 		t.setBackground(new Color(10, 34, 100));
 		f.add(t);
-		
+
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setSize(800, 600);
-		
-		
-		
+	}
+	public static void reset(){
+		f.remove(t);
+		t = new TestJPanel();
+		f.add(t);
+		t.setBackground(new Color(10, 34, 100));
+		f.revalidate();
+		f.repaint();
+	}
+	public static void main(String [] args)
+	{
+		setup();
 	}
 
 }
